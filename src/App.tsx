9dc2209a06618de1aaaa92f5a1430f51263029b6
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-11-21 23:01:59 +0500

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Formik, Form, Field, ErrorMessage } from 'formik'
 
-function App() {
+interface FormValues {
+  name: string
+}
+
+const initialValues: FormValues = {
+  name: ''
+}
+
+const App = () => {
+  const onSubmit = (values: FormValues) => {
+    console.log(values)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Form>
+        <div>
+          <label>Name</label>
+          <Field  as='input' />
+          <ErrorMessage name='name'/>
+        </div>
+      </Form>
+    </Formik>
   );
 }
 
