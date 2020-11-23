79d62ed9dc73de07c3c36ba5a6b71c49401f8f79
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-11-23 22:44:50 +0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
 
 interface FormValues {
   name: string
@@ -10,20 +11,25 @@ const initialValues: FormValues = {
   name: ''
 }
 
+const nameSchema = Yup.object().shape({
+  name: Yup.string().required()
+})
+
 const App = () => {
   const onSubmit = (values: FormValues): void => {
     console.log(values)
   }
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {(props) => {
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={nameSchema}>
+      {({dirty, isValid}) => {
         return (
           <Form>
         <div>
           <label>Name</label>
           <Field  as='input' name='name'/>
           <ErrorMessage name='name'/>
-        </div>
+            </div>
+            <button disabled={!dirty || !isValid} type="submit">Sign Up</button>
       </Form>)}}
     </Formik>
   );
