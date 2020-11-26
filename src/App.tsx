943fe4166417fa766784ca16f6c08d025892a681
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-11-26 23:45:12 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,22 @@ interface FormValues {
   first_name: string;
   last_name: string;
   email: string;
+  password: string
 }
 
 const initialValues: FormValues = {
   first_name: "",
   last_name: "",
   email: "",
+  password: ''
 };
 
 const nameSchema = Yup.object().shape({
   first_name: Yup.string().required(),
   last_name: Yup.string().required(),
   email: Yup.string().required(),
+  password: Yup.string().required(),
+
 });
 
 const App = () => {
@@ -39,6 +43,7 @@ const App = () => {
               <FormField label="First Name" name="first_name" />
               <FormField label="Last Name" name="last_name"/>
               <FormField label="Email" name="email"/>
+              <FormField label="Password" name="password"/>
               <button disabled={!dirty || !isValid} type="submit">
                 Sign Up
               </button>
