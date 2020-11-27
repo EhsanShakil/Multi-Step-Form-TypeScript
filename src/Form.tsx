e2efa3cf425c86b97c2419fcc829a78499cac138
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-11-27 23:23:47 +0500

    Updating

---
+++ src/Form.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./App.css";
+import { Formik, Form} from "formik";
+import * as Yup from "yup";
+import FormField from "./FormField";
+
+interface FormValues {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string
+}
+
+const initialValues: FormValues = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  password: ''
+};
+
+const nameSchema = Yup.object().shape({
+  first_name: Yup.string().required(),
+  last_name: Yup.string().required(),
+  email: Yup.string().required(),
+  password: Yup.string().required(),
+
+});
+
+const App = () => {
+  const onSubmit = (values: FormValues): void => {
+    alert(JSON.stringify(values));
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={nameSchema}
+    >
+      {({ dirty, isValid }) => {
+        return (
+          <Form>
+            <div className="form"> 
+              <FormField label="First Name" name="first_name" />
+              <FormField label="Last Name" name="last_name"/>
+              <FormField label="Email" name="email"/>
+              <FormField label="Password" name="password"/>
+              <button disabled={!dirty || !isValid} type="submit">
+                Sign Up
+              </button>
+            </div>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default App;
